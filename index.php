e19a139ff8a04e997aa04ba903c82bf430ef032f
ouzhigang <ouzhiganggba@gmail.com>
2017-03-12 16:43:41 +0800

    init

--- index.php
+++ index.php
@@ -5,11 +5,14 @@ require("ez_sql/ez_sql_pdo.php");
 require("ez_sql_pdo_cluster.php");
 
 $db = new ezSQL_pdo_cluster(array(
-	array(
+	//写的数据库
+	array(		
 		"dsn" => "mysql:host=127.0.0.1;dbname=test",
 		"user" => "root",
 		"pwd" => "root"
 	),
+	
+	//下面的全部是用来读的
 	array(
 		"dsn" => "mysql:host=127.0.0.1;dbname=test2",
 		"user" => "root",
